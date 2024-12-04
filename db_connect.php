861ba605a87cbb97638c40315347ec64006930e6
sharmasaravanan <Secure@1109>
2024-12-04 17:12:03 +0530

    asset folder

---
+++ db_connect.php
@@ -0,0 +1,3 @@
+<?php 
+
+$conn= new mysqli('x23216093-devops.cizdihoh0eru.us-east-1.rds.amazonaws.com','admin','Admin.1234','laundry_db')or die("Could not connect to mysql".mysqli_error($con));
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Free Download Image Used Links
+-https://www.svgrepo.com/svg/60614/laundry
